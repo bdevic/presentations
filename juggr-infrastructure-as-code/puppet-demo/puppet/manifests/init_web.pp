6edf36c5c12e26394962bef0d0248795ee406c22
Bruno Devic <bdevic.dm@gmail.com>
2014-12-10 23:06:13 +0100

    juggr - infrastructure as code

---
+++ juggr-infrastructure-as-code/puppet-demo/puppet/manifests/init_web.pp
@@ -0,0 +1,20 @@
+exec { 'set_document_root':
+  command => 'ln -fs /vagrant/web-root/* /var/www',
+  onlyif => '[ ! -h /var/www ]',
+  path => '/usr/bin:/bin',
+}
+
+file { '/var/www/':
+  ensure => 'directory',
+}
+
+exec { 'apt-get update':
+  path => '/usr/bin'
+}
+
+package { ['python-software-properties', 'vim']:
+  ensure => present,
+  require => Exec['apt-get update'],
+}
+
+include nginx
